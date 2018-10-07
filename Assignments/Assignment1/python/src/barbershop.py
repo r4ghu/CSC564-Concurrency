9e6b510aac0e07b494662f15091a7c2581d790ff
r4ghu <m.sriraghu@gmail.com>
2018-10-07 12:55:19 -0700

    Modifications

--- Assignments/Assignment1/python/src/barbershop.py
+++ Assignments/Assignment1/python/src/barbershop.py
@@ -1,193 +1,89 @@
-# from utils import Semaphore
+from utils import execution_manager, Semaphore, Thread
+import time, random 
 
-# n = 4
-# customers = 0
-# mutex = Semaphore(1)
-# barber = Semaphore(0)
-# customerDone = Semaphore(0)
-# barberDone = Semaphore(0)
+# a. Use semaphores to simulate a (sleepy) barbershop.
+#
+#    A barbershop holds N customers, and M sleepy barbers 
+#    work on the customers. Correctness criteria are:
+#
+#    (a) no customers should enter the barbershop unless
+#        there is room for them inside
+#    (b) the barber should cut hair if there is a waiting
+#        customer
+#    (c) customers should enter the barbershop if there is 
+#        room inside
+#
+#    This is a sleepy town with laid back people, so strict 
+#    FIFO ordering of waiting customers is not a requirement.
+#
 
-# def customer(i):
-#     mutex.wait()
-#     if customers == n:
-#         mutex.signal()
-#         balk()
-#     customers += 1
-#     mutex.signal()
+def delay():
+    time.sleep(random.randint(0, 2))
 
-#     customer.signal()
-#     barber.wait()
-
-#     # getHairCut()
-
-#     customerDone.signal()
-#     barberDone.wait()
-
-#     mutex.wait()
-#     customers -= 1
-#     mutex.signal()
-
-# def barber(i):
-#     customer.wait()
-#     barber.signal()
-
-#     # cutHair
-
-#     customerDone.wait()
-#     barberDone.signal()
-
-# Barbershop
-from threading import *
-
-m = 1000
-n = 5  # Max amount of customers
-customers = 0
-
-mutex = Semaphore(1)
-customer = Semaphore(0)
-barber = Semaphore(0)
-customerDone = Semaphore(0)
-barberDone = Semaphore(0)
-
-
-def customer_f():
-    global customers
-    mutex.acquire()
-    if customers == n:
-        print("It's full!")
-        mutex.release()
-        return
-    else:
-        customers += 1
-    mutex.release()
-
-    customer.release()
-    barber.acquire()
-    print("Getting haircut: There are " + str(customers) + " customers.")
-    customerDone.release()
-    barberDone.acquire()
-
-    print("Done")
-    mutex.acquire()
-    if customers > 0:
-        customers -= 1
-    mutex.release()
-
-
-def barber_f():
-    global customers
-    while True:
-        barber.release()
-        customer.acquire()
-        print("Cutting hair")
-        barberDone.release()
-        customerDone.acquire()
-
-
-t_customers = [Thread(target=customer_f, name="Customer " + str(x)) for x in range(m)]
-t_barber = Thread(target=barber_f, name="Barber")
-
-t_barber.start()
-[x.start() for x in t_customers]
-
-# https://github.com/nicolas3470/Python-synchronization/blob/master/q05.py
-
-# from __future__ import with_statement
-# from threading import Thread, Lock, Condition, Semaphore
-# from os import _exit as quit
-# import time, random 
-
-# # a. Use semaphores to simulate a (sleepy) barbershop.
-# #
-# #    A barbershop holds N customers, and M sleepy barbers 
-# #    work on the customers. Correctness criteria are:
-# #
-# #    (a) no customers should enter the barbershop unless
-# #        there is room for them inside
-# #    (b) the barber should cut hair if there is a waiting
-# #        customer
-# #    (c) customers should enter the barbershop if there is 
-# #        room inside
-# #
-# #    This is a sleepy town with laid back people, so strict 
-# #    FIFO ordering of waiting customers is not a requirement.
-# #
-
-# def delay():
-#     time.sleep(random.randint(0, 2))
-
-# class BarberShop:
-#     def __init__(self, numchairs):
-#         self.open_seats = numchairs
-#         self.seats_mutex = Semaphore(1)
-#         self.customers = Semaphore(0)
-#         self.barbers = Semaphore(0)
+class BarberShop:
+    def __init__(self, numchairs):
+        self.open_seats = numchairs
+        self.seats_mutex = Semaphore(1)
+        self.customers = Semaphore(0)
+        self.barbers = Semaphore(0)
         
-#     # check for waiting customers
-#     # if there are none, wait
-#     # if there are waiting customers, signal one 
-#     def barber_readytocut(self):
-#         self.customers.acquire()
-#         self.seats_mutex.acquire()
-#         self.open_seats += 1
-#         self.barbers.release()
-#         self.seats_mutex.release()
+    # check for waiting customers
+    # if there are none, wait
+    # if there are waiting customers, signal one 
+    def barber_readytocut(self):
+        self.customers.wait()
+        self.seats_mutex.wait()
+        self.open_seats += 1
+        self.barbers.signal()
+        self.seats_mutex.signal()
 
-#     # enter the barbershop if all numchairs are not occupied
-#     # returns true if the customer entered successfully, and
-#     # false if he was turned away at the door
-#     def customer_enter(self):
-#         self.seats_mutex.acquire()
-#         if self.open_seats > 0:
-#             self.open_seats -= 1
-#             return True
-#         else:
-#             self.seats_mutex.release()
-#             return False
+    # enter the barbershop if all numchairs are not occupied
+    # returns true if the customer entered successfully, and
+    # false if he was turned away at the door
+    def customer_enter(self):
+        self.seats_mutex.wait()
+        if self.open_seats > 0:
+            self.open_seats -= 1
+            return True
+        else:
+            self.seats_mutex.signal()
+            return False
 
-#     # take a seat and wait until the barber is ready to cut hair
-#     def customer_takeaseat(self):
-#         self.customers.release()
-#         self.seats_mutex.release()
-#         self.barbers.acquire()
+    # take a seat and wait until the barber is ready to cut hair
+    def customer_takeaseat(self):
+        self.customers.signal()
+        self.seats_mutex.signal()
+        self.barbers.wait()
 
-# class Barber(Thread):
-#     def __init__(self, id):
-#         Thread.__init__(self)
-#         self.id = id
 
-#     def run(self):
-#         global barbershop
+def Barber(i):
+    global barbershop
+    while True:
+        print("Barber {}: ready to cut hair".format(i))
+        barbershop.barber_readytocut()
+        print("Barber {}: cutting hair".format(i))
+        delay()
+        print("Barber {}: done cutting hair".format(i))
 
-#         while True:
-#             print ("Barber #%d: ready to cut hair" % self.id)
-#             barbershop.barber_readytocut()
-#             print ("Barber #%d: cutting hair" % self.id)
-#             delay()
-#             print ("Barber #%d: done cutting hair" % self.id)
 
-# class Customer(Thread):
-#     def __init__(self, id):
-#         Thread.__init__(self)
-#         self.id = id
+def Customer(i):
+    global barbershop
+    while True:
+        print ("Customer #%d: has long hair" % i)
+        if barbershop.customer_enter():
+            print ("Customer #%d: entered, taking a seat" % i)
+            barbershop.customer_takeaseat()
+            print ("Customer #%d: got a haircut!" % i)
+        else:
+            print ("Customer #%d: turned away from the door" % i)
+        delay()
 
-#     def run(self):
-#         global barbershop
 
-#         while True:
-#             print ("Customer #%d: has long hair" % self.id)
-#             if barbershop.customer_enter():
-#                 print ("Customer #%d: entered, taking a seat" % self.id)
-#                 barbershop.customer_takeaseat()
-#                 print ("Customer #%d: got a haircut!" % self.id)
-#             else:
-#                 print ("Customer #%d: turned away from the door" % self.id)
-#             delay()
+num_barbers = 3
+num_customers = 6
+num_seats = 3
+barbershop = BarberShop(num_seats)
 
-# NUMBARBERS=3
-# NUMCUSTOMERS=6
-# barbershop = BarberShop(3)
-# for i in range(0, NUMBARBERS):
-#     Barber(i).start()
-# for i in range(0, NUMCUSTOMERS):
-#     Customer(i).start()    
+execution_manager()
+[Thread(Barber, i) for i in range(num_barbers)]
+[Thread(Customer, i) for i in range(num_customers)]
--- Assignments/Assignment1/python/src/utils.py
+++ Assignments/Assignment1/python/src/utils.py
@@ -3,6 +3,8 @@ import os
 import signal
 import sys
 import collections
+import time
+import random
 
 
 class Semaphore(threading.Semaphore):
@@ -31,3 +33,50 @@ def execution_manager():
         print('Keyboard Interrupt: Killing the Program')
         os.kill(child, signal.SIGKILL)
     sys.exit()
+
+
+class Philosopher(threading.Thread):
+    running = True
+
+    def __init__(self, name, leftFork, rightFork):
+        # Initialize the thread
+        threading.Thread.__init__(self)
+        # Initialize the params
+        self.name = name
+        self.leftFork = leftFork
+        self.rightFork = rightFork
+
+    def run(self):
+        self.think()
+
+    def think(self):
+        while(self.running):
+            # Philosopher is thinking
+            time.sleep(random.uniform(3, 13))
+            print('{} is hungry'.format(self.name))
+            self.eat()
+
+    def eat(self):
+        # get_forks()
+        leftFork, rightFork = self.leftFork, self.rightFork
+
+        while self.running:
+            leftFork.acquire(True)
+            locked = rightFork.acquire(False)
+            if locked:
+                break
+
+            leftFork.release()
+            print('{} swaps forks'.format(self.name))
+            leftFork, rightFork = rightFork, leftFork
+        else:
+            return
+
+        self.eating()
+        rightFork.release()
+        leftFork.release()
+
+    def eating(self):
+        print('{} starts eating'.format(self.name))
+        time.sleep(random.uniform(1, 10))
+        print('{} finished eating'.format(self.name))
